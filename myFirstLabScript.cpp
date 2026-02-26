2e546e0063d830856aecdc45cdf9b85adeaf4d66
hande-esgin <153733300+hande-esgin@users.noreply.github.com>
2026-02-26 00:50:16 -1000

    Add basic user input script in C++

---
+++ myFirstLabScript.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    string student_id;
+
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "Hello " << name << "." << endl;
+
+    cout << "What is your Student ID?" << endl;
+    cin >> student_id;
+    cout << "Your ID is " << student_id << "." << endl;
+
+    return 0;
+}
